c76d75a5fe06a2d14d8f61274dc8d7b76078c4ac
abi <abigtrz2021@gmail.com>
2024-01-23 04:01:32 -0800

    lymphocyte and antibody code + 3 stubs (antibody is not working properly right now?)

--- main.js
+++ main.js
@@ -2,9 +2,21 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
+ASSET_MANAGER.queueDownload("bones.png");
+ASSET_MANAGER.queueDownload("LymphocyteEdited.png");
+ASSET_MANAGER.queueDownload("RedBloodCell.png");
+ASSET_MANAGER.queueDownload("SpeedBoost.png");
+ASSET_MANAGER.queueDownload("antibody.png");
+
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
+
+	//gameEngine.addEntity(new Antibody(gameEngine, 20 , 75));
+	gameEngine.addEntity(new Bone(gameEngine));
+	gameEngine.addEntity(new Lymphocyte(gameEngine, 20, 20));
+	gameEngine.addEntity(new RedBloodCell(gameEngine));
+	gameEngine.addEntity(new SpeedBoost(gameEngine));
 
 	gameEngine.init(ctx);
 
