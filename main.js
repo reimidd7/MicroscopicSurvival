73db8167537193832ab45b7fc077604c21292488
Reilly Middlebrooks <reimidd7@uw.edu>
2024-01-23 17:07:00 -0800

    minor fixes

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
-ASSET_MANAGER.queueDownload("./MicroSpritesheet.png");
-ASSET_MANAGER.queueDownload("./cell.png");
 
 ASSET_MANAGER.queueDownload("bones.png");
 ASSET_MANAGER.queueDownload("LymphocyteEdited.png");
 ASSET_MANAGER.queueDownload("RedBloodCell.png");
 ASSET_MANAGER.queueDownload("SpeedBoost.png");
 ASSET_MANAGER.queueDownload("antibody.png");
+ASSET_MANAGER.queueDownload("./MicroSpritesheet.png");
+ASSET_MANAGER.queueDownload("./cell.png");
+
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
@@ -16,7 +17,7 @@ ASSET_MANAGER.downloadAll(() => {
 
 	//gameEngine.addEntity(new Antibody(gameEngine, 20 , 75));
 	gameEngine.addEntity(new Bone(gameEngine));
-	gameEngine.addEntity(new Lymphocyte(gameEngine, 20, 20));
+	gameEngine.addEntity(new Lymphocyte(gameEngine, 300, 20));
 	gameEngine.addEntity(new RedBloodCell(gameEngine));
 	gameEngine.addEntity(new SpeedBoost(gameEngine));
 
