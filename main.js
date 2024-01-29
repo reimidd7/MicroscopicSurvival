d5d1e40c1c26a911918da441cb557aa1afdf3514
Enrique Vargas <enriquev@uw.edu>
2024-01-28 19:30:19 -0800

    Fixed errors on my commits/ up to date

--- main.js
+++ main.js
@@ -11,15 +11,13 @@ ASSET_MANAGER.queueDownload("./MicroSpritesheet.png");
 ASSET_MANAGER.queueDownload("./cellman.png");
 ASSET_MANAGER.queueDownload("./otherTiles.png");
 ASSET_MANAGER.queueDownload("./redtiles.png");
-//ASSET_MANAGER.queueDownload("hudhearts.png");
+ASSET_MANAGER.queueDownload("./hudlives.png");
 
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
 
-	gameEngine.addEntity(new Antibody(gameEngine, 20 , 75));
-	//gameEngine.addEntity(new HUD(gameEngine, 0, 10));
 	gameEngine.addEntity(new Bone(gameEngine, 20, 400));
 	gameEngine.addEntity(new RedBloodCell(gameEngine, 20, 200));
 	gameEngine.addEntity(new SpeedBoost(gameEngine));
@@ -34,12 +32,11 @@ ASSET_MANAGER.downloadAll(() => {
         const randomY = Math.random() * canvas.height;
         gameEngine.addEntity(new Cell(gameEngine, randomX, randomY));
     }
-
-	gameEngine.addEntity(new LevelOne(gameEngine, 0, 0));
 	
+	gameEngine.addEntity(new LevelOne(gameEngine, 0, 0));
+	// gameEngine.addEntity(new HUD(gameEngine));
+
 	gameEngine.init(ctx);
 
 	gameEngine.start();
-	
-	
 });
