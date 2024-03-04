d736c3418f09b6ec60584e7025a810023e0a7c42
abi <abigtrz2021@gmail.com>
2024-03-04 15:17:25 -0800

    updated micro wall collisions for level 2-4 + trying to get most recent changes from  levels and scenemanager into my branch

--- micro.js
+++ micro.js
@@ -13,6 +13,8 @@ class Micro {
 
         this.dead = false;
         this.won = false;
+        this.winner = false;
+        this.levelCount = 1;
 
         this.poweredUpSize = false;
         this.sizeTime = 0;
@@ -22,8 +24,6 @@ class Micro {
         this.stunMine = null;
         this.activeStunMine = false;
         this.stunTime = 0;
-
-        // health decline animation: this.animations = new Animator(this.spritesheet, 0, 75, 65, 60, 1, 0.1 )
 
         this.velocity = { x: 0, y: 0 };
 
@@ -44,9 +44,16 @@ class Micro {
 
         this.gameover = false;
 
+        this.bottomOffset = 0;
+        this.rightOffset = 0;
+
         // Micro's animations
         this.animations = [];
         this.loadAnimations();
+
+        //Explosion
+        this.poweredUpExplode = false;
+        this.explodeTime = 0;
 
 
     };
@@ -112,7 +119,7 @@ class Micro {
     //left wall
     collideLeft() {
         if (this.size == 0) {
-            return (this.x - this.BB.radius) < 2;
+            return (this.x - this.BB.radius) < 1;
         } else if (this.size == 1) {
             return (this.x - this.BB.radius) < -5;
         }
@@ -122,11 +129,20 @@ class Micro {
     //right wall
     collideRight() {
         if (this.size == 0) {
-            return (this.x + this.BB.radius) > 955;
+            if (this.levelCount == 2) {
+                this.rightOffset = 1058;
+            } else {
+                this.rightOffset = 950 + (64 * (this.levelCount - 1));
+            }
         } else if (this.size == 1) {
-            return (this.x + this.BB.radius) > 930;
+            if (this.levelCount == 2) {
+                this.rightOffset = 1030;
+            } else {
+                this.rightOffset = 930 + (64 * (this.levelCount - 1));
+            } 
         }
 
+        return (this.x + this.BB.radius) > this.rightOffset;
     };
 
     //Top wall
@@ -138,15 +154,23 @@ class Micro {
         }
     };
 
-    //fix this for additional levels
     //Bottom wall
     collideBottom() {
         if (this.size == 0) {
-            return (this.y + this.BB.radius) > 712;
+            if (this.levelCount == 2) {
+                this.bottomOffset = 866;
+            } else {
+                this.bottomOffset = 708 + (95 * (this.levelCount - 1));
+            }
         } else if (this.size == 1) {
-            return (this.y + this.BB.radius) > 695;
+            if (this.levelCount == 2) {
+                this.bottomOffset = 856;
+            } else {
+                this.bottomOffset = 695 + (95 * (this.levelCount - 1));
+            } 
         }
 
+        return (this.y + this.BB.radius) > this.bottomOffset;
     };
 
     update() {
@@ -155,22 +179,18 @@ class Micro {
         if (this.dead) {
             this.game.camera.microLives -= 1;
             if (this.game.camera.microLives > 0) {
-
                 this.dead = false;
                 this.healthpoints = this.maxHealth;
                 this.x = PARAMS.CANVAS_WIDTH / 2;
                 this.y = PARAMS.CANVAS_WIDTH / 2;
-                this.game.camera.loadLevel(levelThree, false);
+                // this.game.camera.loadLevel(levelThree, false);
             } else {
-                console.log("dead");
-
                 this.velocity.x = 0;
                 this.velocity.y = 0;
                 this.gameover = true;
-                //add end of game screen here? or in draw()
             }
 
-        } else {
+        } else { 
 
             //if (this.poweredUpSpeed == true || this.poweredUpSize == true || this.activeStunMine) {
                 this.powerUp();
@@ -193,15 +213,15 @@ class Micro {
                 if (this.prevCode != this.key && this.key != null) {
                     this.velocity.x = -this.velocity.x;
                     if (this.collideLeft() && this.size == 0) {
-                        this.x = this.BB.radius + 20;
+                        this.x = this.BB.radius + 3;
                     } else if (this.collideLeft() && this.size == 1) {
                         this.x = this.BB.radius - 2;
                     }
 
                     if (this.collideRight() && this.size == 0) {
-                        this.x = 950 - this.BB.radius;
+                        this.x = this.rightOffset - 2 - this.BB.radius;
                     } else if (this.collideRight() && this.size == 1) {
-                        this.x = 925 - this.BB.radius;
+                        this.x = this.rightOffset - 1 - this.BB.radius;
                     }
 
                     //random direction after hitting wall
@@ -224,9 +244,9 @@ class Micro {
                     }
 
                     if (this.collideBottom() && this.size == 0) {
-                        this.y = 708 - this.BB.radius;
+                        this.y = this.bottomOffset - 2 - this.BB.radius;
                     } else if (this.collideBottom() && this.size == 1) {
-                        this.y = 692 - this.BB.radius;
+                        this.y = this.bottomOffset - 1 - this.BB.radius;
                     }
 
                     //random direction after hitting wall
@@ -240,10 +260,10 @@ class Micro {
                 this.key = this.game.keyCode;
 
             } else {
-                this.x += this.velocity.x * this.game.clockTick;
-                this.y += this.velocity.y * this.game.clockTick;
-                this.updateLastBB();
-                this.updateBB();
+            this.x += this.velocity.x * this.game.clockTick;
+            this.y += this.velocity.y * this.game.clockTick;
+            this.updateLastBB();
+            this.updateBB();
             }
 
 
@@ -335,62 +355,85 @@ class Micro {
                             }
 
                             // Check collisions with bones and redblood cells and lymphocytes
-                             if (entity instanceof Bone || entity instanceof RedBloodCell || entity instanceof Lymphocyte) {
-                            if (this.lastBB.x <= (entity.BB.x - this.BB.radius)) { // Collided with the left
-                                this.x = entity.BB.x - this.BB.radius * 3.5;
-                                if (this.velocity.x > 0) {
-                                    this.velocity.x = 0;
-                                    this.velocity.y = 0;
+                            if (entity instanceof Bone || entity instanceof RedBloodCell) {
+                                if (this.lastBB.x <= (entity.BB.x - this.BB.radius)) { // Collided with the left
+                                    this.x = entity.BB.x - this.BB.radius * 3.5;
+                                    if (this.velocity.x > 0) {
+                                        this.velocity.x = 0;
+                                        this.velocity.y = 0;
+                                    }
+                                    //console.log("left collision");
+                                } else if (this.lastBB.x >= entity.BB.x) { // Collided with the right
+                                    this.x = entity.BB.x + this.BB.radius;
+                                    if (this.velocity.x > 0) {
+                                        this.velocity.x = 0;
+                                        this.velocity.y = 0;
+                                    }
+                                    //console.log("right collision");
                                 }
-                                //console.log("left collision");
-                            } else if (this.lastBB.x >= entity.BB.x) { // Collided with the right
-                                this.x = entity.BB.x + this.BB.radius;
-                                if (this.velocity.x > 0) {
-                                    this.velocity.x = 0;
-                                    this.velocity.y = 0;
+
+                                if (this.lastBB.y >= (entity.BB.y + this.BB.radius)) { // Collided with the bottom
+                                    this.y = entity.BB.y + this.BB.radius;
+                                    if (this.velocity.y > 0) {
+                                        this.velocity.x = 0;
+                                        this.velocity.y = 0;
+                                    }
+                                    //console.log("bottom collision");
+                                } else if (this.lastBB.y <= entity.BB.y) { // Collided with the top
+                                    this.y = entity.BB.y - this.BB.radius * 2.5;
+                                    if (this.velocity.y > 0) {
+                                        this.velocity.x = 0;
+                                        this.velocity.y = 0;
+                                    }
+                                    //console.log("top collision");
+
+
                                 }
-                                //console.log("right collision");
                             }
-
-                            if (this.lastBB.y >= (entity.BB.y + this.BB.radius)) { // Collided with the bottom
-                                this.y = entity.BB.y + this.BB.radius;
-                                if (this.velocity.y > 0) {
-                                    this.velocity.x = 0;
-                                    this.velocity.y = 0;
+                        }
+                        //Make sure that the enemies keep dying while explosion is up
+                        if (this.poweredUpExplode) {
+                            for (const entity of this.game.entities) {
+                                if ((entity instanceof Cell || entity instanceof Lymphocyte) && !entity.dead) {
+                                    const dx = this.x - entity.x;
+                                    const dy = this.y - entity.y;
+                                    const distance = Math.sqrt(dx * dx + dy * dy);
+                                    if (distance < 50) {
+                                        entity.decreaseHealth();
+                                        if (entity.healthpoints <= 0) {
+                                            if (entity instanceof Cell) this.game.camera.cellCount -= 1;
+                                            if (entity instanceof Lymphocyte) this.game.camera.lymphocyteCount -= 1;
+                                            entity.dead = true;
+                                        }
+                                    }
                                 }
-                                //console.log("bottom collision");
-                            } else if (this.lastBB.y <= entity.BB.y) { // Collided with the top
-                                this.y = entity.BB.y - this.BB.radius * 2.5;
-                                if (this.velocity.y > 0) {
-                                    this.velocity.x = 0;
-                                    this.velocity.y = 0;
-                                }
-                                //console.log("top collision");
-
-
                             }
                         }
 
-                            if (entity instanceof Powerup) { //make them last for like 15 seconds only
+                        if (entity instanceof Powerup) { //make them last for like 15 seconds only
+                            entity.removeFromWorld = true;
+                            if (entity.type === "speed") {
                                 entity.removeFromWorld = true;
-                                if (entity.type === "speed") {
-                                    entity.removeFromWorld = true;
 
-                                    this.poweredUpSpeed = true;
-                                    this.speedTime = 0;
-                                } else if (entity.type === "size") {
-                                    entity.removeFromWorld = true;
+                                this.poweredUpSpeed = true;
+                                this.speedTime = 0;
+                            } else if (entity.type === "size") {
+                                entity.removeFromWorld = true;
 
-                                    this.poweredUpSize = true;
-                                    this.sizeTime = 0;
+                                this.poweredUpSize = true;
+                                this.sizeTime = 0;
 
-                                } else if (entity.type === "stun") {
-                                    this.stunTime = 0;
-                                    this.stunMine = new Mine(this.game, this.x, this.y, "stun");
-                                    this.game.addEntity(this.stunMine);
-                                    this.stunMine.active = true;
-                                    this.activeStunMine = true;
-                                }
+                            } else if (entity.type === "stun") {
+                                this.stunTime = 0;
+                                this.stunMine = new Mine(this.game, this.x, this.y, "stun");
+                                this.game.addEntity(this.stunMine);
+                                this.stunMine.active = true;
+                                this.activeStunMine = true;
+                            } else if (entity.type === "explode") {
+                                entity.removeFromWorld = true;
+                                this.poweredUpExplode = true;
+                                this.explodeTime = 0;
+                            }
 
                             this.powerUp();
                         }
@@ -429,8 +472,8 @@ class Micro {
 
         this.healthBar.update(this);
 
-        }
     };
+
 
     drawMinimap(ctx, mmX, mmY) {
         ctx.fillStyle = "Green";
@@ -467,6 +510,24 @@ class Micro {
             ctx.stroke();
         }
 
+        //Drawing the radius of the explosion
+        if (this.poweredUpExplode) {
+            ctx.save();
+            if (this.size == 0) {
+                ctx.translate(this.x - this.game.camera.x + 32, this.y - this.game.camera.y + 30); // Adjusted for center
+            } else if (this.size == 1) {
+                ctx.translate(this.x - this.game.camera.x + 46, this.y - this.game.camera.y + 40); // Adjusted for center
+            }
+            
+            ctx.beginPath();
+            ctx.arc(0, 0, 50, 0, Math.PI * 2);
+            ctx.fillStyle = 'rgba(255, 0, 0, 0.2)';
+            ctx.fill();
+            ctx.strokeStyle = 'red';
+            ctx.stroke();
+            ctx.restore();
+        }
+
     };
 
 
@@ -491,6 +552,31 @@ class Micro {
             this.sizeTime = 0;
             this.poweredUpSize = false;
             //console.log("resetting size");
+        }
+        if (this.poweredUpExplode) {
+
+            if (this.explodeTime >= 420) {
+                this.poweredUpExplode = false;
+                this.explodeTime = 0;
+            } else {
+                // Explode logic
+                for (const entity of this.game.entities) {
+                    if ((entity instanceof Cell || entity instanceof Lymphocyte) && !entity.dead) {
+                        const dx = this.x - entity.x;
+                        const dy = this.y - entity.y;
+                        const distance = Math.sqrt(dx * dx + dy * dy);
+                        if (distance < 50) {
+                            entity.decreaseHealth();
+                            if (entity.healthpoints <= 0) {
+                                if (entity instanceof Cell) this.game.camera.cellCount -= 1;
+                                if (entity instanceof Lymphocyte) this.game.camera.lymphocyteCount -= 1;
+                                entity.dead = true;
+                            }
+                        }
+                    }
+                }
+                this.explodeTime++;
+            }
         }
 
         if (this.activeStunMine == true && this.stunTime < 500) {
